8577a5086be05910bf186e01ac05e6318d9898f8
SHAM <shamarknibbs@gmail.com>
2024-11-11 21:02:50 -0500

    Project Init

---
+++ virtual/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const App = () => {
+  return (
+    <div className="bg-gray-100 p-4">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+};
+
+    
+export default App;
