77a34ed25e6a7bc4890d792f5bc43ad7450d0b86
SHAM <shamarknibbs@gmail.com>
2024-11-16 23:47:57 -0500

    Restructure project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import HeroSection from './components/HeroSection';
+import NavBar from './components/NavBar';
+const App = () => {
+  return (
+    <>
+    <NavBar />
+    <div className="max-w-7xl mx-auto pt-20 px-6">
+    <HeroSection />
+    </div>
+    </>
+  );
+};
+
+
+export default App;
